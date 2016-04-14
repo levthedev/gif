70270b5048998cb52f21d765cdfa772ae0795f7b
Lev Kravinsky <kravinskylev@gmail.com>
2016-04-14 12:48:34 -0400

    add --modern implementation plus various refactors

--- lib/gif.rb
+++ lib/gif.rb
@@ -4,20 +4,18 @@ require 'digest/sha1'
 
 module Gif
   class CLI
-    def initialize tags, loops
+    def initialize tags, loops, modern
       @tags = tags
       @loops = loops.to_i
+      ENV["MODERN_TERMINAL"] = "true" if modern
+      @modern = ENV["MODERN_TERMINAL"] if ENV["MODERN_TERMINAL"] == "true"
     end
 
     def fetch
       Dir.mktmpdir do |tmp|
         Dir.chdir(tmp)
         generate_url
-        create_images
-        rename_files
-        @loops.times { gif_to_terminal }
-        `rm #{@gif_sha}.gif`
-        `rm #{@resized_sha}`
+        generate_gifs
       end
     end
 
@@ -26,17 +24,20 @@ module Gif
     def generate_url
       fetch_url     = "https://api.giphy.com/v1/gifs/random?api_key=dc6zaTOxFJmzC&tag=#{@tags}"
       gif_json      = JSON.parse `curl -s '#{fetch_url}'`
-      @gif_url      = gif_json["data"]["fixed_height_small_url"]
+      original_url = gif_json["data"]["image_original_url"]
+      fixed_height_url = gif_json["data"]["fixed_height_small_url"]
+      @modern ? @gif_url = original_url : @gif_url = fixed_height_url
       @gif_sha      = Digest::SHA1.hexdigest @gif_url
       @resized_sha  = Digest::SHA1.hexdigest @gif_sha
     end
 
     def create_images
-      system "curl -s -o #{@gif_sha}.gif #{@gif_url}"
       width  = `tput cols`
       height = `tput lines`
       wh     = [width, "x", height].join.gsub /\n/,""
-      fix_colors
+      suppress_output do
+        `gifsicle --colors=256 #{@gif_sha}.gif -o #{@gif_sha}.gif")`
+      end
       system "gifsicle --resize-fit #{wh} --unoptimize #{@gif_sha}.gif -o #{@resized_sha}"
       system "gifsicle --explode #{@resized_sha} -o frames.jpg"
     end
@@ -44,24 +45,34 @@ module Gif
     def rename_files
       filenames = Dir.glob "*.jpg.*"
       filenames.each_with_index do |filename, i|
-          File.rename filename, "frames-" + i.to_s
+        File.rename filename, "frames-" + i.to_s
       end
     end
 
     def gif_to_terminal
       frames = `ls -1 frames-* | wc -l`.to_i
       frames.times do |n|
-        system "imgcat -R frames-#{n}"
+        system "/usr/local/Cellar/imgcat/1.1.0/bin/imgcat -R frames-#{n}"
         sleep 1.0/24
-        `rm frames-#{n}` if @loops < 1
         system "⌘ k"
       end
       @loops -= 1
     end
 
-    def fix_colors
-      suppress_output do
-        `gifsicle --colors=256 #{@gif_sha}.gif -o #{@gif_sha}.gif")`
+    def gif_to_modern_terminal
+      width  = `tput cols`.to_i
+      height = `tput lines`.to_i
+      system "imgcat -w #{width} -h #{height} #{@gif_sha}.gif"
+    end
+
+    def generate_gifs
+      system "curl -s -o #{@gif_sha}.gif #{@gif_url}"
+      if @modern
+        gif_to_modern_terminal
+      else
+        create_images
+        rename_files
+        @loops.times { gif_to_terminal }
       end
     end
 
